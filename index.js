c0f86780d53d4673292f3d671d4b7c90421ffd96
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-07 23:50:10 +0000

    Removed some console.logs.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { nextISSTimesForMyLocation } = require('./iss');
 
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
-    console.log("I can see my house from here.")
     const datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
     const duration = pass.duration;
